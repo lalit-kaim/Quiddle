a62a4a5b477df68477a03c30b5a01e7df16684e9
Lalit Kaim <kaim.lalit1234@gmail.com>
2020-05-15 19:39:45 +0530

    Add files via upload

---
+++ precache-manifest.76ecdbc12d98d8ef889bd90c48d161e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63c4d85ec18939b1a566",
+    "url": "/static/css/main.b369ee73.chunk.css"
+  },
+  {
+    "revision": "63c4d85ec18939b1a566",
+    "url": "/static/js/main.6d220bae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1dbf7296a47170015f18",
+    "url": "/static/js/2.55f46f47.chunk.js"
+  },
+  {
+    "revision": "dce80c0f8ce1385d19284855d4096343",
+    "url": "/static/media/quiddle.dce80c0f.svg"
+  },
+  {
+    "revision": "56ebf9ad6f611da59fb828cdc10a326a",
+    "url": "/static/media/cover.56ebf9ad.JPG"
+  },
+  {
+    "revision": "04bc3928a39977bb04a54cfa5fb0756d",
+    "url": "/static/media/quiddle.04bc3928.PNG"
+  },
+  {
+    "revision": "58e56506fb72cb36473b163e688bd8d9",
+    "url": "/index.html"
+  }
+];
